d57a1f663280e2d7b769cb9da501297db7186989
ecEthanCar <ec.ethancar@gmail.com>
2023-08-14 12:44:01 -0400

    Added a more information to the interactive map.

--- maps_v2.R
+++ maps_v2.R
@@ -1,17 +1,22 @@
+
+library(readr)
+# big_for_map <- read_csv("data/estate_property_debt_gdp_location(2016-2022).csv")
+# View(estate_property_debt_gdp_location_2016_2022_)
+
 # Define UI
 ui <- fluidPage(
   titlePanel("Interactive Map with Year Dropdown"),
-  selectInput("year_selector", "Select Year:", choices = sort(bigger_3$year)),
-  selectInput("gdp_industry_selector", "Select Industry:", choices = sort(bigger_3$`GDP: Industry`)),
-  selectInput("province_selector", "Select Province:", choices = c("All Provinces", sort(bigger_3$Province))),
-  selectInput("city_selector", "Select City:", choices = c("None", sort(bigger_3$Region))),
+  selectInput("year_selector", "Select Year:", choices = sort(bigger_4$year)),
+  selectInput("gdp_industry_selector", "Select Industry:", choices = sort(bigger_4$`GDP: Industry`)),
+  selectInput("province_selector", "Select Province:", choices = c("All Provinces", sort(bigger_4$Province))),
+  selectInput("city_selector", "Select City:", choices = c("None", sort(bigger_4$Region))),
   leafletOutput("map")
 )
 
 # Define server logic
 server <- function(input, output) {
   output$map <- renderLeaflet({
-    filtered_data <- bigger_3
+    filtered_data <- bigger_4
 
     if (input$province_selector != "All Provinces") {
       filtered_data <- filtered_data |>
@@ -33,11 +38,12 @@ server <- function(input, output) {
         lat = ~Latitude,
         popup = ~paste(
           "Region: ", Region,
-          " , Province: ", Province
+          " , Province: ", Province,
+          " ,  Year: ", year
           # Figure out how to add other column data other than Region, Province, and year
           # " , Administrative Division Code: ", `Debt Ratio(%)`
           ),
-        clusterOptions = markerClusterOptions())
+        clusterOptions = markerClusterOptions(minZoom = 1, maxZoom = 10))
   })
 }
 
